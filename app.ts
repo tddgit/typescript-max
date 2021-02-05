04d1ea8bb2028b1e9755f9af3bfa427f6acad91a
Stas Lifanov <holidayonmail@gmail.com>
2021-02-05 23:24:46 +0300

    Project created

--- app.ts
+++ app.ts
@@ -1,6 +1,11 @@
 function add(n1: number, n2: number) {
+  if (typeof n1 !== "number") {
+    throw new Error("Incorrect type");
+  }
+
+  console.log(typeof n1);
   return n1 + n2;
 }
 
-console.log(add(23, 45));
+console.log(add(56, 45));
 console.log("result");
