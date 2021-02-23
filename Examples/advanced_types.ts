384c9624b82db3f8ea9d70c5316e9538f9ea5fb4
Stas <holidayonmail@gmail.com>
2021-02-23 14:47:08 +0300

    Generics

--- Examples/advanced_types.ts
+++ Examples/advanced_types.ts
@@ -89,7 +89,7 @@ let hellomars: "hello mars" = "hello mars";
 foo(hellomars);
 
 const yObj = {
-    foo: "hello",
+  foo: "hello",
 };
 
 let z: number | string;
@@ -102,18 +102,18 @@ aa.push(33);
 
 let aaa = [6];
 let bb: [number, string, string, number] = [
-    123,
-    "Fake Street",
-    "Nowhere US",
-    10110,
+  123,
+  "Fake Street",
+  "Nowhere US",
+  10110,
 ];
 // bb = [1, 2, 3];
 bb.push();
 
 let cc: { houseNumber: number; streetName: string };
 cc = {
-    streetName: "Fake Street",
-    houseNumber: 123,
+  streetName: "Fake Street",
+  houseNumber: 123,
 };
 
 // cc = {
@@ -122,37 +122,37 @@ cc = {
 
 let dd: { houseNumber: number; streetName?: string };
 dd = {
-    houseNumber: 123,
-    // x=5
+  houseNumber: 123,
+  // x=5
 };
 
 export interface HasPhoneNumber {
-    name: string;
-    phone: number;
+  name: string;
+  phone: number;
 }
 
 export interface HasEmail {
-    name: string;
-    email: string;
+  name: string;
+  email: string;
 }
 
 let contactInfo: HasEmail | HasPhoneNumber =
-    Math.random() > 0.5
-        ? {
-            name: "Mike",
-            phone: 22558456,
-        }
-        : {
-            name: "Mike",
-            email: "mike@example.com",
-        };
+  Math.random() > 0.5
+    ? {
+        name: "Mike",
+        phone: 22558456,
+      }
+    : {
+        name: "Mike",
+        email: "mike@example.com",
+      };
 
 contactInfo.name;
 
 let otherContactInfo: HasEmail & HasPhoneNumber = {
-    name: "Mike",
-    email: "mike@example.com",
-    phone: 55555555,
+  name: "Mike",
+  email: "mike@example.com",
+  phone: 55555555,
 };
 otherContactInfo.name;
 otherContactInfo.phone;
@@ -161,73 +161,73 @@ otherContactInfo.email;
 function validateInputField(input: HTMLInputElement) {}
 
 function sendEmail(to: HasEmail): { recipient: string; body: string } {
-    return {
-        recipient: `${to.name}<${to.email}`,
-        body: "Youre prequalified for the loan!",
-    };
+  return {
+    recipient: `${to.name}<${to.email}`,
+    body: "Youre prequalified for the loan!",
+  };
 }
 
 const sendTextMessage = (
-    to: HasPhoneNumber
+  to: HasPhoneNumber
 ): { recipient: string; body: string } => {
-    return {
-        recipient: `${to.name}<${to.phone}`,
-        body: "Youre prequalified for the loan!",
-    };
+  return {
+    recipient: `${to.name}<${to.phone}`,
+    body: "Youre prequalified for the loan!",
+  };
 };
 
 function getNameParts(contact: { name: string }) {
-    const parts = contact.name.split(/\s/g);
-    if (parts.length === 1) {
-        return { name: parts[0] };
-    }
+  const parts = contact.name.split(/\s/g);
+  if (parts.length === 1) {
+    return { name: parts[0] };
+  }
 
-    if (parts.length) {
-        throw new Error(`Can't caluculate name parts from name`);
-    }
-    return {
-        first: parts[0],
-        middle:
-            parts.length === 2
-                ? undefined
-                : parts.slice(1, parts.length - 2).join(""),
-        last: parts[parts.length - 1],
-    };
+  if (parts.length) {
+    throw new Error(`Can't caluculate name parts from name`);
+  }
+  return {
+    first: parts[0],
+    middle:
+      parts.length === 2
+        ? undefined
+        : parts.slice(1, parts.length - 2).join(""),
+    last: parts[parts.length - 1],
+  };
 }
 
 function contactPeople(method: "email", ...people: HasEmail[]): void;
 function contactPeople(method: "phone", ...people: HasPhoneNumber[]): void;
 function contactPeople(
-    method: "email" | "phone",
-    ...people: (HasEmail | HasPhoneNumber)[]
+  method: "email" | "phone",
+  ...people: (HasEmail | HasPhoneNumber)[]
 ): void {
-    if (method === "email") {
-        (people as HasEmail[]).forEach(sendEmail);
-    } else {
-        (people as HasPhoneNumber[]).forEach(sendTextMessage);
-    }
+  if (method === "email") {
+    (people as HasEmail[]).forEach(sendEmail);
+  } else {
+    (people as HasPhoneNumber[]).forEach(sendTextMessage);
+  }
 }
 contactPeople("email", { name: "foo", email: "" });
 contactPeople("phone", { name: "foo", phone: 1225444 });
 // contactPeople("email", { name: "foo", phone: 1225444 });
 
 function sendMessage(
-    this: HasEmail & HasPhoneNumber,
-    prefferedMethod: "phone" | "email"
+  this: HasEmail & HasPhoneNumber,
+  prefferedMethod: "phone" | "email"
 ) {
-    if (prefferedMethod === "email") {
-        console.log("sendEmail");
-        sendEmail(this);
-    } else {
-        console.log("sendTextMessage");
-        sendTextMessage(this);
-    }
+  if (prefferedMethod === "email") {
+    console.log("sendEmail");
+    sendEmail(this);
+  } else {
+    console.log("sendTextMessage");
+    sendTextMessage(this);
+  }
 }
 
 const c = { name: "Mike", phone: 3388899, email: "mike@example.com" };
 
 function invokeSoon(cb: () => any, timeout: number) {
-    setTimeout(() => cb.call(null), timeout);
+  setTimeout(() => cb.call(null), timeout);
 }
 
 // invokeSoon(() => sendMessage("email"), 500);
@@ -246,33 +246,33 @@ type NumVal = 1 | 2 | 3 | NumArr;
 type NumArr = NumVal[];
 
 export interface HasInternationalPhoneNumber extends HasPhoneNumber {
-    contryCode: string;
+  contryCode: string;
 }
 
 interface ContactMessanger1 {
-    (contact: HasEmail | HasPhoneNumber, message: string): void;
+  (contact: HasEmail | HasPhoneNumber, message: string): void;
 }
 
 type ContactMessanger2 = (
-    contact: HasEmail | HasPhoneNumber,
-    message: string
+  contact: HasEmail | HasPhoneNumber,
+  message: string
 ) => void;
 
 const emailer: ContactMessanger1 = (_contact, _message) => {
-    //
+  //
 };
 
 interface ContactConstructor {
-    new (...args: any[]): HasEmail | HasPhoneNumber;
+  new (...args: any[]): HasEmail | HasPhoneNumber;
 }
 
 interface PhoneNumberDict {
-    [numberName: string]:
-        | undefined
-        | {
+  [numberName: string]:
+    | undefined
+    | {
         areaCode: number;
         num: number;
-    };
+      };
 }
 
 // const d: PhoneNumberDict = {};
@@ -281,19 +281,19 @@ interface PhoneNumberDict {
 // }
 
 const phoneDict: PhoneNumberDict = {
-    office: { areaCode: 321, num: 5551212 },
-    home: { areaCode: 321, num: 555442 },
+  office: { areaCode: 321, num: 5551212 },
+  home: { areaCode: 321, num: 555442 },
 };
 
 interface PhoneNumberDict {
-    home: {
-        areaCode: number;
-        num: number;
-    };
-    office: {
-        areaCode: number;
-        num: number;
-    };
+  home: {
+    areaCode: number;
+    num: number;
+  };
+  office: {
+    areaCode: number;
+    num: number;
+  };
 }
 
 phoneDict.home;
@@ -301,46 +301,46 @@ phoneDict.office;
 phoneDict.mobile;
 
 export class Contact implements HasEmail, HasPhoneNumber {
-    protected age = 0;
-    private passwordVal: string | undefined;
-    constructor(
-        public email: string,
-        public name: string = "no email",
-        public phone: number
-    ) {
-        this.age = 35;
-        if (phone > 0) {
-        }
+  protected age = 0;
+  private passwordVal: string | undefined;
+  constructor(
+    public email: string,
+    public name: string = "no email",
+    public phone: number
+  ) {
+    this.age = 35;
+    if (phone > 0) {
     }
-    get password(): string {
-        if (!this.passwordVal) {
-            this.passwordVal = Math.round(Math.random() * 1e14).toString(32);
-        }
-        return this.passwordVal;
+  }
+  get password(): string {
+    if (!this.passwordVal) {
+      this.passwordVal = Math.round(Math.random() * 1e14).toString(32);
     }
-    //   async init() {
-    //     this.password = Math.round(Math.random() * 1e14).toString(32);
-    //   }
+    return this.passwordVal;
+  }
+  //   async init() {
+  //     this.password = Math.round(Math.random() * 1e14).toString(32);
+  //   }
 }
 
 abstract class AbstractContact implements HasEmail, HasPhoneNumber {
-    public abstract phone: number;
-    constructor(public name: string, public email: string) {}
+  public abstract phone: number;
+  constructor(public name: string, public email: string) {}
 
-    abstract sendEmail(): void;
+  abstract sendEmail(): void;
 }
 
 class ConcreteContact extends AbstractContact {
-    constructor(public phone: number, name: string, email: string) {
-        super(name, email);
-    }
-    sendEmail() {
-        console.log("sending email");
-    }
+  constructor(public phone: number, name: string, email: string) {
+    super(name, email);
+  }
+  sendEmail() {
+    console.log("sending email");
+  }
 }
 
 interface WrappedValue<X> {
-    value: X;
+  value: X;
 }
 
 let val: WrappedValue<string> = { value: "sfadf" };
@@ -349,7 +349,7 @@ val.value;
 const fil = (x: number | string) => true;
 [""].filter(fil);
 interface FilterFunction<T = any> {
-    (val: T): boolean;
+  (val: T): boolean;
 }
 
 //FUNDAMENTALS
@@ -358,9 +358,9 @@ let medals: string[] = ["gold", "silver", "bronze"];
 let [first, second, third] = medals;
 
 let person = {
-    name: "Audrey",
-    address: "123 Main Street",
-    phone: "555-1212",
+  name: "Audrey",
+  address: "123 Main Street",
+  phone: "555-1212",
 };
 
 let { name, address, phone } = person;
@@ -370,18 +370,18 @@ let { name, address, phone } = person;
 // }
 
 interface WrappedValue<X> {
-    value: X;
+  value: X;
 }
 
 let val1: WrappedValue<string> = { value: "" };
 val1.value;
 
 interface FilterFunction<T = any> {
-    (val: T): boolean;
+  (val: T): boolean;
 }
 
 const stringFilter: FilterFunction<string> = (val: string) =>
-    typeof val === "string";
+  typeof val === "string";
 // stringFilter(0);
 stringFilter("abc");
 
@@ -393,59 +393,59 @@ stringFilter("abc");
 // truthyFilter(["abc"]);
 
 function resolveOrTimeout<T>(promise: Promise<T>, timeout: number): Promise<T> {
-    return new Promise<T>((resolve, reject) => {
-        const task = setTimeout(() => reject("time up!"), timeout);
+  return new Promise<T>((resolve, reject) => {
+    const task = setTimeout(() => reject("time up!"), timeout);
 
-        promise.then((val) => {
-            clearTimeout(task);
-            resolve(val);
-        });
+    promise.then((val) => {
+      clearTimeout(task);
+      resolve(val);
     });
+  });
 }
 
 // resolveOrTimeout(fetch(""), 3000);
 
 function arrayToDict<T extends { id: string }>(array: T[]): { [k: string]: T } {
-    const out: { [k: string]: T } = {};
-    array.forEach((val) => {
-        out[val.id] = val;
-    });
-    return out;
+  const out: { [k: string]: T } = {};
+  array.forEach((val) => {
+    out[val.id] = val;
+  });
+  return out;
 }
 
 const myDict = arrayToDict([
-    { id: "a", value: "first", lisa: "Huang" },
-    { id: "b", value: "second" },
+  { id: "a", value: "first", lisa: "Huang" },
+  { id: "b", value: "second" },
 ]);
 
 function startTuple<T>(a: T) {
-    return function finishTuple<U>(b: U) {
-        return [a, b] as [T, U];
-    };
+  return function finishTuple<U>(b: U) {
+    return [a, b] as [T, U];
+  };
 }
 
 const myTuple = startTuple(["first"])(42);
 console.log(myTuple);
 
 type Shape = {
-    draw(): void;
-    isDrawn: boolean;
+  draw(): void;
+  isDrawn: boolean;
 };
 
 interface Circle extends Shape {
-    radius: number;
+  radius: number;
 }
 
 function drawShapes1<S extends Shape>(shapes: S[]) {
-    shapes.map((s) => {
-        s.draw();
-        s.isDrawn = true;
-        return s;
-    });
+  shapes.map((s) => {
+    s.draw();
+    s.isDrawn = true;
+    return s;
+  });
 }
 
 function drawShapes2(shapes: Shape[]) {
-    shapes.forEach((s) => s.draw());
+  shapes.forEach((s) => s.draw());
 }
 
 const cir: Circle = { draw() {}, radius: 4, isDrawn: false };
@@ -475,60 +475,60 @@ let myAny: any = 32;
 let myUnknown: unknown = "hello, unknown";
 
 async function logWhenResolved(p: Promise<any>) {
-    const val = await p;
-    console.log("Resolved to: ", val);
+  const val = await p;
+  console.log("Resolved to: ", val);
 }
 
 if (typeof myUnknown === "string") {
-    myUnknown.split(",");
+  myUnknown.split(",");
 }
 
 if (myUnknown instanceof Promise) {
-    myUnknown.then((x) => console.log(x));
+  myUnknown.then((x) => console.log(x));
 }
 
 function isHasEmail(x: any): x is HasEmail {
-    return typeof x.name === "string" && typeof x.email === "string";
+  return typeof x.name === "string" && typeof x.email === "string";
 }
 
 if (isHasEmail(myUnknown)) {
-    console.log(myUnknown.name, myUnknown.email);
+  console.log(myUnknown.name, myUnknown.email);
 }
 
 function isDefined<T>(arg: T | undefined): arg is T {
-    return typeof arg !== "undefined";
+  return typeof arg !== "undefined";
 }
 
 const list = ["a", "b", "c", undefined, "e", "f"];
 
 const filtered = list.filter(isDefined);
 {
-    let aaaa: unknown = 41;
-    let bbb: unknown = ["a", "string", "array"];
-    // bb = aa;
+  let aaaa: unknown = 41;
+  let bbb: unknown = ["a", "string", "array"];
+  // bb = aa;
 }
 interface BrandedA {
-    __this_is_branded_with_a: "a";
+  __this_is_branded_with_a: "a";
 }
 
 function brandA(value: string): BrandedA {
-    return (value as unknown) as BrandedA;
+  return (value as unknown) as BrandedA;
 }
 
 function unbrandA(value: BrandedA): string {
-    return (value as unknown) as string;
+  return (value as unknown) as string;
 }
 
 interface BrandedB {
-    __this_is_branded_with_b: "b";
+  __this_is_branded_with_b: "b";
 }
 
 function brandB(value: { abc: string }): BrandedB {
-    return (value as unknown) as BrandedB;
+  return (value as unknown) as BrandedB;
 }
 
 function unbrandB(value: BrandedB): { abc: string } {
-    return (value as unknown) as { abc: string };
+  return (value as unknown) as { abc: string };
 }
 
 let secretA = brandA("This is a secret value");
@@ -564,9 +564,3 @@ unbrandA(secretB);
 // } else {
 //   throw new UnreachabaleError(y_new, "y should be a string" + " or number");
 // }
-
-interface CommunicationMethods {
-    email: HasEmail;
-    phone: HasPhoneNumber;
-    fax: { fax: number };
-}
--- app.ts
+++ app.ts
@@ -1,3 +1,10 @@
+import { HasEmail, HasPhoneNumber } from "./Examples/advanced_types";
+
+interface CommunicationMethods {
+  email: HasEmail;
+  phone: HasPhoneNumber;
+  fax: { fax: number };
+}
 
 function contact<K extends keyof CommunicationMethods>(
   method: K,
@@ -13,3 +20,62 @@ type AllCommValues = CommunicationMethods[keyof CommunicationMethods];
 
 const alreadyResolvedNum = Promise.resolve(4);
 type ResolveType = typeof Promise.resolve;
+
+type MayHaveEmail = Partial<HasEmail>;
+const me: MayHaveEmail = {};
+
+type PickA = Pick<{ a: 1; b: 2 }, "a">;
+
+type OnlyStrings = Extract<"a" | "b" | 1 | 2, number>;
+
+function foo() {}
+interface bar {}
+namespace baz {
+  export const biz = "hello";
+}
+
+const x = foo;
+
+const y: bar = {};
+
+let z: bar;
+
+baz;
+
+export { foo, bar, baz };
+
+const xx = 4;
+const yy: typeof xx = 4;
+
+interface Address {
+  street: string;
+}
+
+// const z = Address;
+
+class Contact {
+  name: string,
+  static hello = 'world'
+}
+
+Contact.hello;
+
+const contactClass = Contact;
+const contactInstance: Contact = new Contact();
+
+
+class Album {
+  label: Album.AlbumLabel = new Album.AlbumLabel()
+}
+
+namespace Album {
+  export class AlbumLabel {}
+}
+
+interface Album {
+  artist: string;
+}
+
+let al: Album;
+
+export  { Album }
